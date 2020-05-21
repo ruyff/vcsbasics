b6efd40bb220b54e30668b5b985370bb502fbd89
Marcin Cichon | pk systems GmbH <mcichon@pksystems.net>
2020-05-21 22:12:16 +0200

    Initial 2

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,8 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World dos!");
+            //comment
         }
     }
 }
